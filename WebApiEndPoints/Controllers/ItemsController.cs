6564744c110299b40fc1d97a4ae90a81442f3089
NeethuKPushkaran <141960227+NeethuKPushkaran@users.noreply.github.com>
2024-04-09 09:46:20 +0530

    'first-commit-

---
+++ WebApiEndPoints/Controllers/ItemsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiEndPoints.Model;
+
+namespace WebApiEndPoints.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ItemsController : ControllerBase
+    {
+        public static List<Item> _item = new List<Item>
+        {
+            new Item { Id = 1, Title = "Weather", Description = "Weather changes unexpectadly"},
+            new Item { Id = 2, Title = "Temparature", Description = "Temperature Increases in Summer"}
+        };
+
+        [HttpGet]
+
+        public IActionResult GetItems()
+        {
+
+        }
+    }
+}
---
+++ WebApiEndPoints/Model/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiEndPoints.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
